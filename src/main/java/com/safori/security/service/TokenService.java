c12a5eb5578a33850018622c44c36fddc5b47098
hann <wjdgks1233@gmail.com>
2026-06-17 11:07:18 +0900

    Feat : use refreshtoken #48

--- src/main/java/com/safori/security/service/TokenService.java
+++ src/main/java/com/safori/security/service/TokenService.java
@@ -6,7 +6,13 @@ import org.springframework.security.core.Authentication;
 public interface TokenService {
     JwtToken login(String username, String password);
 
+    JwtToken reissueToken(String refreshToken);
+
     JwtToken generateToken(Authentication authentication);
 
     Authentication getAuthentication(String accessToken);
+
+    boolean logout(String refreshToken);
+
+    boolean existsRefreshToken(String refreshToken);
 }
