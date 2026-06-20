9d05254c62cf8326665b991523143addfedef6eb
YoonSeo Kong <ehcl1027@gmail.com>
2026-06-20 17:01:41 +0900

    feat: add JWT bearer auth scheme

--- src/main/java/com/safori/common/config/SwaggerConfig.java
+++ src/main/java/com/safori/common/config/SwaggerConfig.java
@@ -1,12 +1,17 @@
 package com.safori.common.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
     @Bean
     public OpenAPI openAPI() {
@@ -14,6 +19,14 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("SAFORI API")
                         .description("SAFORI 서비스 API 문서")
-                        .version("v1"));
+                        .version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
